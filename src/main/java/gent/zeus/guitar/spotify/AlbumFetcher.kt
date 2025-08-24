309062a1061238fc28b726303ec3e28232f188d9
lolcatjpg <66800621+lolcatjpg@users.noreply.github.com>
2025-08-24 18:27:29 +0200

    restructure AlbumFetcher

--- src/main/java/gent/zeus/guitar/spotify/AlbumFetcher.kt
+++ src/main/java/gent/zeus/guitar/spotify/AlbumFetcher.kt
@@ -8,38 +8,31 @@ import org.springframework.web.client.body
 class AlbumFetcher(id: String) : SpotifyFetcher<Album>(id, SpotifyObjectType.ALBUM) {
     override fun fetch(): Album? = makeApiRequest().body<SpotifyAlbumJson>()
         .takeIfNotNullOrLog()?.let { albumJson ->
-            val tracks: List<Track?> = albumJson.tracks?.items?.map { trackJson ->
-                if (trackJson == null) return null
-
-                val artists: List<Artist?> = trackJson.artists.map { artistJson ->
+            Album(
+                albumJson.id,
+                albumJson.name,
+                albumJson.tracks?.items?.map { trackJson ->
+                    if (trackJson == null) return null
+                    Track(
+                        trackJson.id,
+                        trackJson.name,
+                        null,
+                        trackJson.artists.map { artistJson ->
+                            if (artistJson == null) return null
+                            Artist(
+                                artistJson.id,
+                                artistJson.name
+                            )
+                        },
+                    )
+                } ?: emptyList(),
+                albumJson.artists?.map { artistJson ->
                     if (artistJson == null) return null
                     Artist(
                         artistJson.id,
                         artistJson.name
                     )
-                }
-
-                Track(
-                    trackJson.id,
-                    trackJson.name,
-                    null,
-                    artists,
-                )
-            } ?: emptyList()
-
-            val artists: List<Artist?> = albumJson.artists?.map { artistJson ->
-                if (artistJson == null) return null
-                Artist(
-                    artistJson.id,
-                    artistJson.name
-                )
-            } ?: emptyList()
-
-            Album(
-                albumJson.id,
-                albumJson.name,
-                tracks,
-                artists,
+                } ?: emptyList(),
             )
         }
 }
--- src/main/java/gent/zeus/guitar/spotify/ArtistFetcher.kt
+++ src/main/java/gent/zeus/guitar/spotify/ArtistFetcher.kt
@@ -8,7 +8,7 @@ class ArtistFetcher(var id: String) : SpotifyFetcher<Artist>(id, SpotifyObjectTy
         .takeIfNotNullOrLog()?.let { artistJson ->
             Artist(
                 artistJson.id,
-                null //artistJson.name,
+                artistJson.name,
             )
         }
 }
