4828f700feb2fc83dee8a8a220ed6654dcafeca8
lolcatjpg <66800621+lolcatjpg@users.noreply.github.com>
2025-09-03 03:27:00 +0200

    add proper db init script (also main branch is now functional lol)

--- src/main/resources/sqlScripts/init.sql
+++ src/main/resources/sqlScripts/init.sql
@@ -1,5 +1,33 @@
-create table tracks(
-    spotifyId varchar primary key ,
-    name varchar,
-    album varchar,
-)
+create table track
+(
+    track_id   varchar primary key,
+    spotify_id varchar unique,
+    name       varchar,
+    album      varchar,
+    length     int,             -- length in ms
+    play_share double precision -- share of song's own length that is has been played in the kelder
+);
+
+create table track_artist
+(
+    track_id  varchar,
+    artist_id varchar,
+
+    foreign key (track_id) references track (track_id)
+);
+
+create table artist
+(
+    artist_id varchar primary key,
+    spotifyId varchar unique,
+    name      varchar
+);
+
+create table artist_genre
+(
+    artist_id varchar,
+    genre     varchar,
+
+    primary key (artist_id, genre),
+    foreign key (artist_id) references artist (artist_id)
+);
