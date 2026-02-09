4d8d85e674260adf8e884ccdefb3b6af4a92f4c5
lolcatjpg <66800621+lolcatjpg@users.noreply.github.com>
2026-02-09 22:02:50 +0100

    notify when components are disabled because required env variable is not set

--- src/main/kotlin/gent/zeus/guitar/Environment.kt
+++ src/main/kotlin/gent/zeus/guitar/Environment.kt
@@ -11,13 +11,13 @@ object Environment {
     val SPOTIFY_CLIENT_ID = loadRequired("SPOTIFY_CLIENT_ID")
     val SPOTIFY_CLIENT_SECRET = loadRequired("SPOTIFY_CLIENT_SECRET")
 
-    val MQTT_HOST = load("MQTT_HOST")
+    val MQTT_HOST = load("MQTT_HOST".also { notifyDisabled("mqtt", it) })
     val MQTT_PORT = load("MQTT_PORT")
     val MQTT_LIBRESPOT_LISTEN_TOPIC = load("MQTT_LIBRESPOT_LISTEN_TOPIC")
     val MQTT_ZODOM_LISTEN_TOPIC = load("MQTT_ZODOM_LISTEN_TOPIC")
     val MQTT_PUBLISH_TOPIC = load("MQTT_PUBLISH_TOPIC")
 
-    val ZODOM_API_URL = load("ZODOM_API_URL")
+    val ZODOM_API_URL = load("ZODOM_API_URL".also { notifyDisabled("zodom data", it) })
 
     init {
         if (loadFailed) exitProcess(1)
@@ -42,4 +42,9 @@ object Environment {
      * @return the value of the variable, or an empty string if it is not set
      */
     private fun load(key: String): String = System.getenv(key).takeUnless { it?.isEmpty() ?: true } ?: ""
+
+    private fun notifyDisabled(what: String, envVariableKey: String) {
+        if (!load(envVariableKey).isEmpty()) return
+        logger.warn("$what is disabled because environment variable $envVariableKey is not set")
+    }
 }
