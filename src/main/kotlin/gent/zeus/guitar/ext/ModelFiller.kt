33256c0f87b9c4546fc59d6bf76e5334e21da9e4
lolcatjpg <66800621+lolcatjpg@users.noreply.github.com>
2026-02-09 21:50:35 +0100

    update model filler comment

--- src/main/kotlin/gent/zeus/guitar/ext/ModelFiller.kt
+++ src/main/kotlin/gent/zeus/guitar/ext/ModelFiller.kt
@@ -6,7 +6,10 @@ import gent.zeus.guitar.data.MusicModel
 interface ModelFiller<T : MusicModel> {
 
     /**
-     * fetch data and put it in the given MusicalObject
+     * fetch data and put it in the given model
+     *
+     * this should _copy_ the model, not modify it directly.
+     * (use `Model.copy(new values...)`)
      */
     fun fetchInto(musicModel: T): DataResult<T>
 }
