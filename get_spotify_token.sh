b8d976251d6632530341980abc58d1b1dc821525
lolcatjpg <saturn@lolcatjpg.com>
2026-04-02 18:20:12 +0200

    add justfile and remove redundant files in root

--- get_spotify_token.sh
+++
@@ -1,2 +0,0 @@
-SPOTIFY_TOKEN="$(xhs post https://accounts.spotify.com/api/token content-type:application/x-www-form-urlencoded --raw "grant_type=client_credentials&client_id=$SPOTIFY_CLIENT_ID&client_secret=$SPOTIFY_CLIENT_SECRET" | jq .access_token | sed 's/"//g')"
-
